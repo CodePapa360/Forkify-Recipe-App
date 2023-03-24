b44ba32c7729194cfc4d88b5452bd9fd328692cc
Alamin <106429613+CodePapa360@users.noreply.github.com>
2023-03-24 14:08:11 +0600

    Added the recipe selection functionality
    Every time when user selects a recipe from the sidebar, it should stay selected.

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -579,6 +579,8 @@ const controlRecipes = async function() {
         const id = window.location.hash.slice(1);
         if (!id) return;
         (0, _recipeViewJsDefault.default).renderSpinner();
+        // Update results view to mark selected search result
+        (0, _resultsViewJsDefault.default).update(_modelJs.getSearchResultsPage());
         //1) Loading recipe`1
         await _modelJs.loadRecipe(id);
         //2) Rendering recipe
@@ -2874,7 +2876,6 @@ class View {
         this._parentElement.insertAdjacentHTML("afterbegin", markup);
     }
     update(data) {
-        if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const newMarkup = this._generateMarkup();
         const newDom = document.createRange().createContextualFragment(newMarkup);
@@ -3261,9 +3262,10 @@ class ResultsView extends (0, _viewJsDefault.default) {
         return this._data.map(this._generateMarkupPreview).join("");
     }
     _generateMarkupPreview(result) {
+        const id = window.location.hash.slice(1);
         return `
         <li class="preview">
-            <a class="preview__link" href="#${result.id}">
+            <a class="preview__link ${result.id === id ? "preview__link--active" : ""}" href="#${result.id}">
               <figure class="preview__fig">
                 <img src="${result.image}" alt="${result.title}" />
               </figure>
